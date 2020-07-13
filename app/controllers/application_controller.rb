b80b866ddff60451abf69ed32e4503a234716183
Cole Rau <colevrau@gmail.com>
2020-07-13 14:54:10 -0700

    added nav bar in application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    #writing "helper_method" makes method available to views too (along with controllers)
+    helper_method :current_user
+
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id])
+        end 
+    end
 end
