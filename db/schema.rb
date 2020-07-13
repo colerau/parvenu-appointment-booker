0c472eafe6f43b24fc4c8bad7d5e1a64645344b5
Cole Rau <colevrau@gmail.com>
2020-07-13 14:06:10 -0700

    added uid and image column to users, fixed javascript error in application layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_202425) do
+ActiveRecord::Schema.define(version: 2020_07_13_210437) do
 
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_07_13_202425) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_confirmation"
+    t.string "image"
+    t.string "uid"
   end
 
 end
