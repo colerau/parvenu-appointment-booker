af06f50a3c8fc557371f2bc2d8da0c155cc39dac
Cole Rau <colevrau@gmail.com>
2020-07-13 21:33:30 -0700

    appointments#new lookin good, appointments new form works for choosing a stylist

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,9 +1,16 @@
 class AppointmentsController < ApplicationController
     def new 
-        @user = User.find(params[:user_id])
+        @appointment = Appointment.new(user_id: params[:user_id])
+        binding.pry
     end
 
     def create 
         binding.pry
+    end
+
+    private 
+
+    def appointment_params 
+        params.require(:appointment)
     end
 end
