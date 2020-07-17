ebd2cbe68e3fb7e265b0d39f48a2c6154b4dd43d
Cole Rau <colevrau@gmail.com>
2020-07-16 21:35:56 -0700

    fixed bug where user's appointment's employee was always Karen

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,17 +7,15 @@ class AppointmentsController < ApplicationController
         @appointment = Appointment.new(appointment_params)
         if @appointment.save 
             flash[:notice] = "Appointment successfully booked"
-            redirect_to user_appointment_path(@appointment.id, @appointment.user_id)
+            redirect_to user_appointment_path(@appointment.user_id, @appointment.id)
         else 
             flash[:notice] = "Something went wrong"
             render :new 
         end
     end
 
-    def show 
-        binding.pry
+    def show
         @appointment = Appointment.find_by(id: params[:id])
-        binding.pry
         @user = User.find(@appointment.user_id)
     end
 
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,12 +2,4 @@ class Appointment < ApplicationRecord
     belongs_to :user
     belongs_to :employee
 
-    validate :yeet 
-
-    private 
-
-    def yeet 
-        binding.pry 
-        byebug
-    end
 end
