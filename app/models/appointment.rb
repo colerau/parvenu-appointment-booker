677c10865c25834d8994d934d626ffbf924bfc98
Cole Rau <colevrau@gmail.com>
2020-07-17 16:25:02 -0700

    error messages readable

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,13 +6,13 @@ class Appointment < ApplicationRecord
 
     def valid_time
         if time.hour % 2 == 0
-            errors.add(:time, "Hour must be an odd number")
+            errors.add(:time, "hour must be an odd number")
         end
         if time.hour < 9 || time.hour > 17
-            errors.add(:time, "Hour must be between 9 and 17")
+            errors.add(:time, "hour must be between 9 and 17")
         end
         if time.strftime("%M") != "00"
-            errors.add(:time, "Minute of hour must be 00")
+            errors.add(:time, "minute of hour must be 00")
         end 
     end
 end
