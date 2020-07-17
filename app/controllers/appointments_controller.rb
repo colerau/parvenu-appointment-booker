67f7bff39728217ca33345660feb276f1a2dc356
Cole Rau <colevrau@gmail.com>
2020-07-16 21:01:23 -0700

    user can book appointment and see their appointment (C & R of CRUD complete)

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,7 +5,20 @@ class AppointmentsController < ApplicationController
 
     def create 
         @appointment = Appointment.new(appointment_params)
+        if @appointment.save 
+            flash[:notice] = "Appointment successfully booked"
+            redirect_to user_appointment_path(@appointment.id, @appointment.user_id)
+        else 
+            flash[:notice] = "Something went wrong"
+            render :new 
+        end
+    end
+
+    def show 
         binding.pry
+        @appointment = Appointment.find_by(id: params[:id])
+        binding.pry
+        @user = User.find(@appointment.user_id)
     end
 
     private 
