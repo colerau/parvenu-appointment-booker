6d9c9d6086dbe100f06cb6587d0b86476ce0047f
Cole Rau <colevrau@gmail.com>
2020-07-17 13:01:38 -0700

    valid_time method in appt validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,17 @@ class Appointment < ApplicationRecord
     belongs_to :user
     belongs_to :employee
 
+    validate :valid_time
+
+    def valid_time
+        if time.hour % 2 == 0
+            errors.add(:time, "Hour must be an odd number")
+        end
+        if time.hour < 9 || time.hour > 17
+            errors.add(:time, "Hour must be between 9 and 17")
+        end
+        if time.strftime("%M") != "00"
+            errors.add(:time, "Minute of hour must be 00")
+        end 
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
     has_many :appointments
     has_many :employees, through: :appointments
-    
+
     has_secure_password
 
     validates :email, presence: true, uniqueness: true 
-
     validates :password, confirmation: true 
-
 end
