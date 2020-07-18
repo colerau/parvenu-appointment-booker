4856b360e43b9e000d786bf25b6612a92422f17d
Cole Rau <colevrau@gmail.com>
2020-07-17 21:31:29 -0700

    is_users_appointment? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,25 @@ class ApplicationController < ActionController::Base
     #writing "helper_method" makes method available to views too (along with controllers)
     helper_method :current_user, :authorize
 
+    def logged_in?
+        !!session[:user_id]
+    end
+
     def current_user
         if session[:user_id]
             @current_user ||= User.find(session[:user_id])
         end 
     end
 
+    # * probably don't need this method
     def authorize
         if current_user.nil? 
             flash[:notice] = "You must be logged in to access this page"
             redirect_to login_path
         end
+    end
+
+    def is_users_appointment?(appointment)
+        current_user.id == appointment.user_id
     end
 end
